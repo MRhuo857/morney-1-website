3f5749076e3f7ffec708134d07f2ef262c0e3886
mr.huo <455801665@qq.com>
2020-10-31 13:46:32 +0800

    deploy

---
+++ precache-manifest.59585be774adeaf0e9e7a9133e0826d0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f37f8ceb595d44b1bfce544b268344b",
+    "url": "/morney-1-website/index.html"
+  },
+  {
+    "revision": "ca13f8483fec1c17c1d2",
+    "url": "/morney-1-website/static/css/main.a18f5f5c.chunk.css"
+  },
+  {
+    "revision": "62cb61d49d4431af80f3",
+    "url": "/morney-1-website/static/js/2.b1268502.chunk.js"
+  },
+  {
+    "revision": "de7c95d42723c2d4f76d99ad9a65a751",
+    "url": "/morney-1-website/static/js/2.b1268502.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca13f8483fec1c17c1d2",
+    "url": "/morney-1-website/static/js/main.8316dc74.chunk.js"
+  },
+  {
+    "revision": "fae83565c968e581bfcc",
+    "url": "/morney-1-website/static/js/runtime-main.db7f7e8b.js"
+  }
+]);
